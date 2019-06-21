5afbb1cf400f5633b9dc0e2a892908df15119e58
Gavin.Z <moranxichen@live.com>
2019-06-21 18:52:31 +0800

    test
    test python

---
+++ Python/AlarmClock.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import tkinter.messagebox
+import time
+
+def Rupdate():
+    now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    TimeLabel.config(text=now_time)
+    AC_Window.after(1000,Rupdate)
+
+AC_Window = tk.Tk()
+AC_Window.title ("AlarmClock")
+AC_Window.geometry("200x100")
+AC_Window.minsize(100,100)
+AC_Window.wm_attributes('-topmost',1)
+AC_Window.config(bg = "black")
+
+TimeLabel = tk.Label(AC_Window,text = "Time",font = ("微软雅黑",10),fg = "white" ,bg = "black")
+TimeLabel.pack()
+
+Rupdate()
+AC_Window = tk.mainloop()
+
